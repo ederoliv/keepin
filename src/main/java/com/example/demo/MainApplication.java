7832e55d3a741add4fa7acde593b2eb75b8ed143
p41nwt55 <ederoliveira152@gmail.com>
2024-03-19 13:51:12 -0300

    Excluindo imports desnecessários

--- src/main/java/com/example/demo/MainApplication.java
+++ src/main/java/com/example/demo/MainApplication.java
@@ -1,28 +1,19 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class MainApplication extends Application {
-
-    @FXML
-    private ComboBox<String> comboBoxRecebidoPor;
 
     public static final String DATABASE_URL = "jdbc:ucanaccess://G://Meu Drive//Projetos//Projeto Supp//supp-project//src//main//resources//supp_db.accdb";
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("KeepIn - Hebron Software");
+        stage.setTitle("KeepIn - Desenvolvido por: Eder Oliveira");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
